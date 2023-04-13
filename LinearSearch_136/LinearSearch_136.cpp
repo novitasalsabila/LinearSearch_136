0fff76631674cb0c78efde690da969dc50d8f138
novitasalsabila <novnovv10@gmail.com>
2023-04-13 14:39:26 +0700

    Menggunakan prosedure input

--- LinearSearch_136/LinearSearch_136.cpp
+++ LinearSearch_136/LinearSearch_136.cpp
@@ -5,4 +5,18 @@ int arr[20];	//Array to be searted
 int n;	//Number of element in array
 int i;	//Index of array element
 
+void input()
+{
+	while (true)
+	{
+		cout << "Enter the number of element in the array :";
+		cin >> n;
+		if ((n > 0) && (n <= 20))
+			break;
+		else
+			cout << "\nArray should have minimum 1 and maxsimum 20 element.\n\n";
+	};
+
+
+}
 
